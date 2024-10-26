c41de5db8d294249609281be359d00ce6ec29b69
Michael <147973061+Limkor@users.noreply.github.com>
2024-10-26 20:47:59 +0300

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ val logback_version: String by project
 plugins {
     kotlin("jvm") version "2.0.21"
     id("io.ktor.plugin") version "3.0.0"
+    id("org.jetbrains.dokka") version "1.8.10"
 }
 
 group = "com.example"
@@ -28,6 +29,10 @@ dependencies {
     implementation("io.ktor:ktor-server-netty-jvm")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-config-yaml")
+    implementation("mysql:mysql-connector-java:8.0.28")
     testImplementation("io.ktor:ktor-server-test-host-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    implementation("com.zaxxer:HikariCP:5.0.1")
+    implementation("mysql:mysql-connector-java:8.0.28")
+    dokkaPlugin("com.glureau:html-mermaid-dokka-plugin:0.3.2")
 }
